3e6e59d3f5c2be87307ec6b9e3a04d2605decd55
Gfarf <gfpalu@gmail.com>
2025-08-12 11:13:52 -0300

    unit tests added

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,44 @@
+package auth
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestAuth(t *testing.T) {
+	tests := map[string]struct {
+		header     string
+		identifier string
+		want       string
+		errorWant  string
+	}{
+		"Correct Key":      {header: "Authorization", identifier: "ApiKey ItisaCorrectKey", want: "ItisaCorrectKey", errorWant: ""},
+		"Incorrect Header": {header: "", identifier: "ApiKey ItisaCorrectKey", want: "", errorWant: "no authorization header"},
+		"Malformed Key":    {header: "Authorization", identifier: "Api wrong key", want: "", errorWant: "malformed authorization header"},
+		"Malformed Key 2 ": {header: "Authorization", identifier: "ApiKey", want: "", errorWant: "malformed authorization header"},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			res := &http.Response{
+				Header: http.Header{},
+			}
+			res.Header.Set(tc.header, tc.identifier)
+			got, err := GetAPIKey(res.Header)
+			if err != nil {
+				if strings.Contains(err.Error(), tc.errorWant) {
+					return
+				}
+				t.Errorf("Unexpected: TestGetAPIKey:%v\n", err)
+				return
+			} else {
+				diff := cmp.Diff(tc.want, got)
+				if diff != "" {
+					t.Fatalf(diff)
+				}
+			}
+		})
+	}
+}
